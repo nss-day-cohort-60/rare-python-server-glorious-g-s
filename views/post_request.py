cdf194ab3c481c286b9c7ca18a887fb779516942
Jordon Perkins <jordon.rubel@gmail.com>
2023-01-31 15:56:30 -0600

    created the get all posts

--- views/post_request.py
+++ views/post_request.py
@@ -2,6 +2,47 @@ import sqlite3
 import json
 from models import Post
 
+def get_all_posts():
+    # Open a connection to the database
+    with sqlite3.connect("./loaddata.sqlite3") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.user_id,
+            a.title,
+            a.publication_date,
+            a.image_url,
+            a.content,
+            a.approved
+        FROM posts a
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        posts = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row.
+            # Note that the database fields are specified in
+            # exact order of the parameters defined in the
+            # Animal class above.
+            post = Post(row['id'], row['user_id'], row["title"], row["publication_date"], row["image_url"],
+                        row['content'], row['approved'])
+
+            posts.append(post.__dict__)
+
+    return posts
+
 def get_single_post(id):
     with sqlite3.connect("./loaddata.sqlite3") as conn:
         conn.row_factory = sqlite3.Row
