690546ad31e187b59e9fe399b8d1bd3aaaac8f56
Ricky Canepa <rickycanepa.rc@gmail.com>
2023-01-31 15:31:10 -0600

    working on comments

---
+++ views/comment_request.py
@@ -0,0 +1,52 @@
+import sqlite3
+import json
+import models
+
+def get_all_comments():
+    # Open a connection to the database
+    with sqlite3.connect("./kennel.sqlite3") as conn:
+
+        # Just use these. It's a Black Box.
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        # Write the SQL query to get the information you want
+        db_cursor.execute("""
+        SELECT
+            a.id,
+            a.post_id,
+            a.author_id,
+            a.content,
+            b.
+        FROM Animal a
+        JOIN Post b
+            ON l.id = a.post_id
+        JOIN Customer c
+            ON c.id = a.customer_id
+        """)
+
+        # Initialize an empty list to hold all animal representations
+        animals = []
+
+        # Convert rows of data into a Python list
+        dataset = db_cursor.fetchall()
+
+        # Iterate list of data returned from database
+        for row in dataset:
+
+            # Create an animal instance from the current row
+            animal = Animal(row['id'], row['name'], row['breed'], row['status'],
+                            row['location_id'], row['customer_id'])
+
+            # Create a Location instance from the current row
+            location = Location(row['id'], row['location_name'], row['location_address'])
+            customer = Customer(row['id'], row['customer_name'], row['customer_address'], row['customer_email'])
+
+            # Add the dictionary representation of the location to the animal
+            animal.location = location.__dict__
+            animal.customer = customer.__dict__
+
+            # Add the dictionary representation of the animal to the list
+            animals.append(animal.__dict__)
+
+    return animals
