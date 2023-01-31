792afad2ad8fa98bfc1928cc57fa72dcb584f56f
Byron <byron.glenn86@yahoo.com>
2023-01-31 13:52:06 -0600

    added data

--- loaddata.sql
+++ loaddata.sql
@@ -11,16 +11,6 @@ CREATE TABLE "Users" (
   "active" bit
 );
 
-CREATE TABLE "DemotionQueue" (
-  "action" varchar,
-  "admin_id" INTEGER,
-  "approver_one_id" INTEGER,
-  FOREIGN KEY(`admin_id`) REFERENCES `Users`(`id`),
-  FOREIGN KEY(`approver_one_id`) REFERENCES `Users`(`id`),
-  PRIMARY KEY (action, admin_id, approver_one_id)
-);
-
-
 CREATE TABLE "Subscriptions" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "follower_id" INTEGER,
@@ -33,13 +23,12 @@ CREATE TABLE "Subscriptions" (
 CREATE TABLE "Posts" (
   "id" INTEGER PRIMARY KEY AUTOINCREMENT,
   "user_id" INTEGER,
-  "category_id" INTEGER,
   "title" varchar,
   "publication_date" date,
   "image_url" varchar,
   "content" varchar,
   "approved" bit,
-  FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`),
+  FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`)
 );
 
 CREATE TABLE "Comments" (
@@ -51,40 +40,11 @@ CREATE TABLE "Comments" (
   FOREIGN KEY(`author_id`) REFERENCES `Users`(`id`)
 );
 
-CREATE TABLE "Reactions" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "label" varchar,
-  "image_url" varchar
-);
+INSERT INTO `Users` VALUES (null, "Pat", "Mahomes", "pat@pat.com", "Football player", "pat", "pat", "https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTU3ODc4NjAwMDI2ODkxNTkz/the-nfl-begins-football-grass-2014-hero-2.jpg", 01312023, 1);
+INSERT INTO `Users` VALUES (null, "Jalen", "Hurts", "hurts@hurts.com", "Football player", "jalen", "jalen", "https://assets3.cbsnewsstatic.com/hub/i/r/2022/03/14/08c8764e-029d-4bf2-8319-db05c67a20d3/thumbnail/640x392/be38c4b9e67216d7a78628552724738c/tom-brady-football.jpg", 01312023, 1);
+INSERT INTO `Subscriptions` VALUES (null, 1, 2, 01312023);
+INSERT INTO `Posts` VALUES (NULL, 1, 2, "How to win a superbowl", 02072023, "https://www.si.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc0NDU1OTQyMjAzNjQ3NjIy/college-football-covid-symptoms-cases-players.jpg", "You can win a superbowl by outscoring your opponent.", 1);
+INSERT INTO `Comments` VALUES (NULL, 1, 2, "That is not how you win a superbowl.");
 
-CREATE TABLE "PostReactions" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "user_id" INTEGER,
-  "reaction_id" INTEGER,
-  "post_id" INTEGER,
-  FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`),
-  FOREIGN KEY(`reaction_id`) REFERENCES `Reactions`(`id`),
-  FOREIGN KEY(`post_id`) REFERENCES `Posts`(`id`)
-);
-
-CREATE TABLE "Tags" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "label" varchar
-);
-
-CREATE TABLE "PostTags" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "post_id" INTEGER,
-  "tag_id" INTEGER,
-  FOREIGN KEY(`post_id`) REFERENCES `Posts`(`id`),
-  FOREIGN KEY(`tag_id`) REFERENCES `Tags`(`id`)
-);
-
-CREATE TABLE "Categories" (
-  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
-  "label" varchar
-);
-
-INSERT INTO Categories ('label') VALUES ('News');
-INSERT INTO Tags ('label') VALUES ('JavaScript');
-INSERT INTO Reactions ('label', 'image_url') VALUES ('happy', 'https://pngtree.com/so/happy');
+DELETE FROM Posts 
+WHERE id >= 2
