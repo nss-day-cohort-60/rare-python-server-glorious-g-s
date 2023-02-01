9bf2afae1f0318abda0f09725e313199bd7fcaf1
Christy Le <lechrsty@gmail.com>
2023-02-01 09:40:50 -0800

    added queries for comments and users, switched out parse URL code block, renamed user.py

--- views/user_requests.py
+++ views/user_requests.py
@@ -62,9 +62,36 @@ def get_single_user(id):
         data = db_cursor.fetchone()
 
         user = User(data['id'], data['first_name'], data['last_name'], data['email'],
-                                data['bio'], data['username'], data['password'], data['profile_image_url'],data['created_on'], data['active'])
-        
+                                data['bio'], data['username'], data['password'], data['profile_image_url'], data['created_on'], data['active'])
 
         return user.__dict__
 
 
+def get_user_by_username(username):
+
+    with sqlite3.connect("./loaddata.sqlite3") as conn:
+        conn.row_factory = sqlite3.Row
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            u.id,
+            u.first_name,
+            u.last_name,
+            u.email,
+            u.bio,
+            u.username,
+            u.password,
+            u.profile_image_url,
+            u.created_on,
+            u.active
+        from Users u
+        WHERE u.username = ?
+        """, (username,))
+
+        data = db_cursor.fetchone()
+
+        user = User(data['id'], data['first_name'], data['last_name'], data['email'],
+                                data['bio'], data['username'], data['password'], data['profile_image_url'], data['created_on'], data['active'])
+
+        return user.__dict__
